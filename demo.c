d564a82977b4bee53b8fc396feeff6ae0e73b610
lxy1103039412 <1103039412@qq.com>
2020-12-02 16:37:12 +0800

    https://github.com/lxy1103039412/gdb_test

--- demo.c
+++ demo.c
@@ -3,6 +3,6 @@ int main(void){
     int input = 0;
     printf("input an interger:\n");
     scanf("%d",&input);
-    printf("the interger you input is %d\n",input);
+    printf("the interger you input iss %d\n",input);
     return 0;
 }
